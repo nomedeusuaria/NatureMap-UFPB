0af2d3deade9192282bf4059ebc43c90896fce05
Sarah Marina Lopes Ribeiro <sarah.lopes@labs.dcx>
2023-04-17 17:41:30 -0300

    conectando o servidor

---
+++ src/app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+//app.use(cors());
+//app.use(express.json());
+
+const app = express();
+
+module.exports = app;
